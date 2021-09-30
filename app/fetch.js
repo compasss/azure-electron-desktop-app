05fd782b50b74cac3d7f86531f559dc37a92947a
compasss <crazymingzai@gmail.com>
2021-09-30 17:06:04 +0800

    init project

---
+++ app/fetch.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+/**
+ * Makes an Authorization 'Bearer' request with the given accessToken to the given endpoint.
+ * @param endpoint
+ * @param accessToken
+ */
+async function callEndpointWithToken(endpoint, accessToken) {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  };
+
+  console.log('Request made at: ' + new Date().toString());
+
+  const response = await axios.default.get(endpoint, options);
+
+  return response.data;
+}
+
+module.exports = {
+  callEndpointWithToken: callEndpointWithToken,
+};
